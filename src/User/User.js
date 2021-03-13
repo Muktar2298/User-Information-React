1dd6fca5bf08d98633956590c10440acbacdb0ae
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-03-14 00:58:01 +0600

    User-Information

--- src/User/User.js
+++ src/User/User.js
@@ -6,14 +6,12 @@ import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 // --user style import --//
 import './User.css';
-import UserDetails from '../UserDetails/UserDetails';
 
 const User = (props) => {
     const {name,username,img,phone} = props.user;
     return (
         <div className='user-container'>
-            <UserDetails></UserDetails>
-
+        
             <img src={img} alt=""/>
             <h3>Name: {name}</h3>
             <button > <FontAwesomeIcon icon={faUserPlus} /> </button>
